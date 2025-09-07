10da188627fbada182d5a565e7621cdd6d42e895
pythonking888 <j_koon888@hotmail.com>
2025-09-07 17:26:39 +0100

    Add files via upload

---
+++ def baby_love().py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hello World", layout="centered")
+
+st.title("👋 Hello, Streamlit World!")
+st.write("This is your first Streamlit app. Welcome aboard!")
