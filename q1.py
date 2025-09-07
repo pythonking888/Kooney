2e462cc772ba78eb5368b4134642958de0127e2d
pythonking888 <j_koon888@hotmail.com>
2025-09-07 17:30:57 +0100

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# Set page config for dark mode
+st.set_page_config(page_title="Leo vs Daddy", layout="centered")
+
+# Inject custom CSS for black background and white text
+st.markdown("""
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+        .stApp {
+            background-color: black;
+            color: white;
+        }
+        h1, h2, h3, h4, h5, h6, p, label {
+            color: white !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Title and question
+st.title("Who Do You Love More? ❤️")
+choice = st.radio("Choose one:", ["Leo", "Daddy"])
+
+# Display image based on choice
+if choice == "Leo":
+    st.image("https://raisingchildren.net.au/newborns/behaviour/understanding-behaviour/newborn-behaviour", caption="Leo 👶")
+elif choice == "Daddy":
+    st.image("https://heroes-and-villain.fandom.com/wiki/Hulk_(Marvel_Cinematic_Universe)", caption="Daddy 💪 (Hulk)")
